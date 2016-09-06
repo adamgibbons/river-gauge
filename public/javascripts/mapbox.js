20f096c5a071240054dc9f32d54fa85d29605598
adamgibbons <adam.d.gibbons@gmail.com>
2016-09-05 21:05:53 -0600

    adds markers to layer; handles hover, click events

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -9,42 +9,61 @@ var formatStation = function formatStation(station) {
     },
     "properties": {
       "title": station.name,
-      "marker-symbol": "marker",
-      "marker-size": "large",
-      "marker-color": "#1087bf"
+      "icon": "circle",
+      "marker-color": "#aaaaaa"
     }
   };
 }
 
-
 $.get('/api/stations')
-.done(function (response) {
+.then(function (response) {
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
+    style: 'mapbox://styles/adamgibbons/ciorsxlby000lb3khbf02yrkn', //stylesheet location
     center: [-105.26, 40.03], // starting position
     zoom: 12 // starting zoom
   });
   map.on('load', function () {
-    map.addSource("markers", {
+
+    map.addSource("stations", {
       "type": "geojson",
       "data": {
         "type": "FeatureCollection",
         "features": response.stations.map(formatStation)
       }
     });
+
     map.addLayer({
-      "id": "markers",
+      "id": "stations",
       "type": "symbol",
-      "source": "markers",
+      "source": "stations",
       "layout": {
-        "icon-image": "{marker-symbol}-15",
+        "icon-image": "{icon}-15",
         "text-field": "{title}",
         "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
         "text-offset": [0, 0.6],
         "text-anchor": "top"
       }
     });
+
+    map.on('click', function (e) {
+        // Use queryRenderedFeatures to get features at a click event's point
+        // Use layer option to avoid getting results from other layers
+        var features = map.queryRenderedFeatures(e.point, { layers: ['stations'] });
+        // if there are features within the given radius of the click event,
+        // fly to the location of the click event
+        if (features.length) {
+          console.log(features);
+            // Get coordinates from the symbol and center the map on those coordinates
+            map.flyTo({center: features[0].geometry.coordinates});
+        }
+    });
+
+    map.on('mousemove', function (e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ['stations'] });
+        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+    });
+
   });
 })
 .fail(function (err) {
